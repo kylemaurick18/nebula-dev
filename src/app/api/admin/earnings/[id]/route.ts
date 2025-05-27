f8faa547a210250c2b9cdc117f42a8b26d37ea7c
Kyle Maurick <ultimateuser18@gmail.com>
2025-05-27 19:13:02 +0100

    fix deployment issue new

--- src/app/api/admin/earnings/[id]/route.ts
+++ src/app/api/admin/earnings/[id]/route.ts
@@ -2,7 +2,9 @@ import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
 import prisma from '@/lib/prisma'
 
-export async function DELETE(req: Request, context: any) {
+import { NextRequest } from 'next/server'
+
+export async function DELETE(req: NextRequest, context: any) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
 
   if (!token || token.role !== 'ADMIN') {
