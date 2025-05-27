5e2d1ed03ea961de52b8f22ddf56688e4676052e
Kyle Maurick <ultimateuser18@gmail.com>
2025-05-27 01:25:43 +0100

    fix deployment issue

--- src/app/api/activities/route.ts
+++ src/app/api/activities/route.ts
@@ -1,8 +1,9 @@
+import { NextRequest } from 'next/server'
 import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
 import prisma from '@/lib/prisma'
 
-export async function GET(req: Request) {
+export async function GET(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
   if (!token || !token.email) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
