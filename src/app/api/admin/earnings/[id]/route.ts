cddae5f67de76a93106dac84c66b39af205d2a08
Kyle Maurick <ultimateuser18@gmail.com>
2025-05-27 01:10:45 +0100

    fix deployment issue

--- src/app/api/admin/earnings/[id]/route.ts
+++ src/app/api/admin/earnings/[id]/route.ts
@@ -2,18 +2,17 @@ import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
 import prisma from '@/lib/prisma'
 
-export async function DELETE(
-  req: Request,
-  context: { params: Record<string, string> }
-) {
+export async function DELETE(req: Request, context: any) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
 
   if (!token || token.role !== 'ADMIN') {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
   }
 
-  await prisma.earning.delete({
-    where: { id: context.params.id },
+  const { id } = context.params
+
+  await prisma.activity.delete({
+    where: { id },
   })
 
   return NextResponse.json({ success: true })
