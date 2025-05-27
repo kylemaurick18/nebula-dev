d44832bdf48d3a9461a78d07a22e7ed4653a1596
Kyle Maurick <ultimateuser18@gmail.com>
2025-05-27 19:19:35 +0100

    fix deployment issue new

--- src/app/api/admin/users/route.ts
+++ src/app/api/admin/users/route.ts
@@ -1,8 +1,8 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import { getToken } from 'next-auth/jwt'
 import prisma from '@/lib/prisma'
 
-export async function GET(req: Request) {
+export async function GET(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
 
   if (!token || token.role !== 'ADMIN') {
